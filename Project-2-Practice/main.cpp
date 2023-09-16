ff84065f54693f7e4451d342996e19e1ac6bd1eb
bhaskar10h <112790780+bhaskar10h@users.noreply.github.com>
2023-09-16 15:45:45 +0530

    Update main.cpp
    Upgrade the code with
    /*
    Menu Options:
    1. Create Account
    2. Login
    3. Exit
    Enter your choice:
    */

--- Project-2-Practice/main.cpp
+++ Project-2-Practice/main.cpp
@@ -1,170 +1,202 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-class bankServer {
+class BankAccount {
 private:
-    int optn;
-    string person;
-    double amount;
+    string accountNumber;
+    string password;
     double balance;
+    int accountType; // 1: Saving, 2: Business, 3: Industrial
 
 public:
-    void personBalance(double bal) {
-        this->balance = bal;
+    BankAccount(string accNum, string pass, int type) {
+        accountNumber = accNum;
+        password = pass;
+        balance = 0.0;
+        accountType = type;
     }
 
-    void window() {
-        cout << "Your Account type : " << endl;
-        cout << "--------------------" << endl;
-        cout << "1. Saving Account " << endl;
-        cout << "2. Business Account " << endl;
-        cout << "3. Industrial Account " << endl;
+    string getAccountNumber() {
+        return accountNumber;
     }
 
-    void accHolder(double amount, int option) {
-        this->amount = amount;
-        this->optn = option;
-        cout << "Enter your option : ";
-        cin >> option;
-        switch (option) {
-        case 1:
-            cout << "From our Database, your account is of 'Saving Account' type " << endl;
-            break;
-        case 2:
-            cout << "From our Database, your account is of 'Business Account' type " << endl;
-            break;
-        case 3:
-            cout << "From our Database, your account is of 'Industrial Account' type " << endl;
-            break;
-        default:
-            cout << "Invalid Option. Please choose from the list which is displayed on the window" << endl;
-            break;
-        }
+    bool verifyPassword(string pass) {
+        return password == pass;
+    }
 
-        if (option == 1) {
-            double interest;
-            interest = 7.33 / 100;
-            balance = (balance + amount) * interest;
-            cout << "Your Balance in saving account is : ";
-            cout << balance << endl;
-            return;
-        }
-        else if (option == 2) {
-            double interest;
-            interest = 11.43 / 100;
-            balance = (balance + amount) * interest;
-            cout << "Your Balance in business account is : ";
-            cout << balance << endl;
-            return;
-        }
-        else if (option == 3) {
-            double interest;
-            interest = 21.65 / 100;
-            balance = (balance + amount) * interest;
-            cout << "Your Balance in industrial account is : ";
-            cout << balance << endl;
-            return;
+    double getBalance() {
+        return balance;
+    }
+
+    int getAccountType() {
+        return accountType;
+    }
+
+    void deposit(double amt) {
+        balance += amt;
+        cout << "You have deposited an amount of $" << amt << ". Your new balance is $" << balance << endl;
+    }
+
+    bool withdraw(double amt) {
+        if (amt <= balance) {
+            balance -= amt;
+            cout << "You have withdrawn an amount of $" << amt << ". Your new balance is $" << balance << endl;
+            return true;
         }
         else {
-            cout << "Can't read from server database." << endl;
+            cout << "Insufficient balance. Your current balance is $" << balance << endl;
+            return false;
         }
     }
 
-    void withdrawls(double amt, string name) {
-        this->amount = amt;
-        this->person = name;
-
-        cout << "Dear Account-holder, " << name << ", you have chosen the withdrawal option: " << endl;
-
-        if (balance >= amount) {
-            balance -= amount;
-            cout << "You have withdrawn an amount of " << amount << " from your account" << endl;
-            cout << "Your Balance is : " << balance << endl;
-            return;
-        }
-        else if (amount > balance) {
-            cout << "You have insufficient balance in your account" << endl;
-            cout << "Your current balance of your account is : " << balance << endl;
-            return;
-        }
-    }
-
-    void deposit(double amt, string name) {
-        this->amount = amt;
-        this->person = name;
-
-        balance += amount;
-        cout << "Dear Account-holder, " << name << ", you have deposited an amount of " << amount << " into your account" << endl;
-        cout << "Your Balance is : " << balance << endl;
-    }
-
-    void displayDetails(string name) {
-        this->person = name;
-
-        cout << "Account Holder Name: " << name << endl;
+    void displayDetails() {
+        cout << "Account Number: " << accountNumber << endl;
         cout << "Account Type: ";
-        switch (optn) {
+        switch (accountType) {
         case 1: cout << "Saving Account" << endl; break;
         case 2: cout << "Business Account" << endl; break;
         case 3: cout << "Industrial Account" << endl; break;
         default: cout << "Unknown" << endl; break;
         }
-        cout << "Current Balance: " << balance << endl;
+        cout << "Current Balance: $" << balance << endl;
     }
 
-    void changeAccountType(int newOption) {
-        this->optn = newOption;
+    void changeAccountType(int newType) {
+        accountType = newType;
         cout << "Account type changed successfully!" << endl;
+    }
+};
+
+class BankServer {
+private:
+    vector<BankAccount> accounts;
+
+public:
+    BankAccount* createAccount(string accNum, string pass, int type) {
+        BankAccount newAccount(accNum, pass, type);
+        accounts.push_back(newAccount);
+        return &accounts.back(); // Return a pointer to the newly created account
+    }
+
+    BankAccount* findAccount(string accNum) {
+        for (size_t i = 0; i < accounts.size(); i++) {
+            if (accounts[i].getAccountNumber() == accNum) {
+                return &accounts[i];
+            }
+        }
+        return nullptr; // Account not found
     }
 
     void showMenu() {
         cout << "Menu Options:" << endl;
-        cout << "1. Display Account Details" << endl;
-        cout << "2. Deposit Money" << endl;
-        cout << "3. Withdraw Money" << endl;
-        cout << "4. Change Account Type" << endl;
-        cout << "5. Exit" << endl;
+        cout << "1. Create Account" << endl;
+        cout << "2. Login" << endl;
+        cout << "3. Exit" << endl;
     }
 
     void handleMenuOption(int option) {
         switch (option) {
-        case 1: displayDetails(person); break;
-        case 2:
-            double depositAmount;
-            cout << "Enter the amount to deposit: ";
-            cin >> depositAmount;
-            deposit(depositAmount, person);
+        case 1: {
+            string accNum, pass;
+            int accType;
+            cout << "Enter Account Number: ";
+            cin >> accNum;
+            cout << "Enter Password: ";
+            cin >> pass;
+            cout << "Enter Account Type (1 for Saving, 2 for Business, 3 for Industrial): ";
+            cin >> accType;
+            createAccount(accNum, pass, accType);
+            cout << "Account created successfully!" << endl;
             break;
+        }
+        case 2: {
+            string inputAccNum, inputPass;
+            cout << "Enter Account Number: ";
+            cin >> inputAccNum;
+            cout << "Enter Password: ";
+            cin >> inputPass;
+            BankAccount* userAccount = findAccount(inputAccNum);
+            if (userAccount != nullptr && userAccount->verifyPassword(inputPass)) {
+                int menuOption;
+                do {
+                    showAccountMenu();
+                    cout << "Enter your choice: ";
+                    cin >> menuOption;
+                    handleAccountMenuOption(userAccount, menuOption);
+                } while (menuOption != 5);
+            }
+            else {
+                cout << "Invalid account number or password. Please try again." << endl;
+            }
+            break;
+        }
         case 3:
+            cout << "Exiting..." << endl;
+            exit(0);
+        default:
+            cout << "Invalid option. Please try again." << endl;
+            break;
+        }
+    }
+
+    void showAccountMenu() {
+        cout << "Account Options:" << endl;
+        cout << "1. Display Account Details" << endl;
+        cout << "2. Deposit Money" << endl;
+        cout << "3. Withdraw Money" << endl;
+        cout << "4. Change Account Type" << endl;
+        cout << "5. Logout" << endl;
+    }
+
+    void handleAccountMenuOption(BankAccount* account, int option) {
+        switch (option) {
+        case 1:
+            account->displayDetails();
+            break;
+        case 2: {
+            double depositAmount;
+            cout << "Enter the amount to deposit: $";
+            cin >> depositAmount;
+            account->deposit(depositAmount);
+            break;
+        }
+        case 3: {
             double withdrawalAmount;
-            cout << "Enter the amount to withdraw: ";
+            cout << "Enter the amount to withdraw: $";
             cin >> withdrawalAmount;
-            withdrawls(withdrawalAmount, person);
+            account->withdraw(withdrawalAmount);
             break;
-        case 4:
-            int newOption;
+        }
+        case 4: {
+            int newType;
             cout << "Enter the new account type (1 for Saving, 2 for Business, 3 for Industrial): ";
-            cin >> newOption;
-            changeAccountType(newOption);
+            cin >> newType;
+            account->changeAccountType(newType);
             break;
-        case 5: cout << "Exiting..." << endl; exit(0);
-        default: cout << "Invalid option. Please try again." << endl;
+        }
+        case 5:
+            cout << "Logging out..." << endl;
+            break;
+        default:
+            cout << "Invalid option. Please try again." << endl;
+            break;
         }
     }
 };
 
 int main() {
-    bankServer my_Info;
-    my_Info.personBalance(100000.00);
+    BankServer bankServer;
 
     int menuOption;
     do {
-        my_Info.showMenu();
+        bankServer.showMenu();
         cout << "Enter your choice: ";
         cin >> menuOption;
-        my_Info.handleMenuOption(menuOption);
-    } while (menuOption != 5);
+        bankServer.handleMenuOption(menuOption);
+    } while (menuOption != 3);
 
     return 0;
 }
